cdcb9fdddd4d0465aa3f5935e54937fadd5075ee
LAMM: MIT Laboratory for Atomistic and Molecular Mechanics <101393859+lamm-mit@users.noreply.github.com>
2024-10-07 19:33:37 -0400

    Update utils.py, cosmetic changes

--- PRefLexOR/utils.py
+++ PRefLexOR/utils.py
@@ -1,3 +1,7 @@
+#####################################################
+# PRefLexOR utility functions
+#####################################################
+
 from llama_index.core.indices.vector_store.retrievers import VectorIndexRetriever
 
 import torch
