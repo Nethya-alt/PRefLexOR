9914964202f4aa1170f2be24f30b5f19c139526e
LAMM: MIT Laboratory for Atomistic and Molecular Mechanics <101393859+lamm-mit@users.noreply.github.com>
2024-10-24 08:33:39 -0400

    Update __init__.py

--- PRefLexOR/__init__.py
+++ PRefLexOR/__init__.py
@@ -5,13 +5,3 @@ from .active_trainer import *
 from .inference import *
 from .utils import *
 
-__all__ = [
-    # Active Trainer functions/classes
-    'train_active_model',  # assuming there are functions like this in active_trainer.py
-    
-    # Inference functions/classes
-    'recursive_response_from_thinking', 
-    
-    # Utils functions/classes
-    'plot_metrics'
-]
