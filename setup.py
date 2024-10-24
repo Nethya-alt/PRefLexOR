507e8450b1a193337f5a96a30f922f96bd731043
LAMM: MIT Laboratory for Atomistic and Molecular Mechanics <101393859+lamm-mit@users.noreply.github.com>
2024-10-24 09:57:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
 
+# Function to read requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as req_file:
+        return req_file.read().splitlines()
+
 setup(
     name='PRefLexOR',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'torch',
-        'transformers',
-        'matplotlib',
-        'tqdm',
-    ],
-    entry_points={
-        'console_scripts': [
-            # Add any console scripts here, if applicable
-        ],
-    },
+    install_requires=parse_requirements('requirements.txt'),
     description='PRefLexOR: Recursive Language Modeling for Exploratory Reasoning Optimization',
-    author='Your Name',
+    author='Markus J. Buehler',
     url='https://github.com/lamm-mit/PRefLexOR',
 )
